9fb5e04b364e2f1bc910f12f0d80d152f8b21c97
Ahmed Farag <afarag@microsoft.com>
2019-10-25 16:35:20 +0000

    Fix comments

--- Scanner/ScannerMain.Designer.cs
+++ Scanner/ScannerMain.Designer.cs
@@ -34,9 +34,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(586, 385);
+            this.button1.Location = new System.Drawing.Point(293, 200);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(202, 53);
+            this.button1.Size = new System.Drawing.Size(101, 28);
             this.button1.TabIndex = 0;
             this.button1.Text = "Exit";
             this.button1.UseVisualStyleBackColor = true;
@@ -44,9 +45,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(37, 45);
+            this.button2.Location = new System.Drawing.Point(18, 23);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(210, 81);
+            this.button2.Size = new System.Drawing.Size(105, 42);
             this.button2.TabIndex = 1;
             this.button2.Text = "Connect to DB";
             this.button2.UseVisualStyleBackColor = true;
@@ -54,13 +56,14 @@
             // 
             // ScannerMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(400, 234);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "ScannerMain";
-            this.Text = "Welcome to Scanner (McAfee)";
+            this.Text = "Welcome to Scanner Software v1.02";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
